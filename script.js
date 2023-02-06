9e5b5fcd1e8300d62d98529e11a6a8dc2886bedf
Pedrobe <109363992+Pedrobe9@users.noreply.github.com>
2023-02-06 22:07:27 +0000

    Adding elements to display information of the weather

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ function geolocation(city){
       url: queryURL,
       method: "GET"
     }).then(function (response) {
-      console.log("response:", response)
+      console.log("response:", response);
       console.log("response ajax:", response[0].lat, response[0].lon);
-      var locationG = [response[0].lat, response[0].lon]
-      //return (location);
+      var locationG = [response[0].lat, response[0].lon, city];
+      //return (locationG);
       weatherDisplay(locationG);
     });
     
@@ -29,12 +29,12 @@ function geolocation(city){
 function weatherDisplay(locationW) {
   
     //var city = $(this).attr("data-name");
-    console.log("weatherlocation function:", locationW);
+    console.log("weatherDisplay:", locationW);
     // define lat and lon
     var lat = locationW[0];
     var lon = locationW[1];
     console.log("lat lon:", lat, lon);
-    //var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={" + lat + "}&lon={" + lon + "}&appid={" + APIkey + "}"
+   
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=73022bce8f98139a77816edb0e805bb7";
     // Creating an AJAX call for the specific movie button being clicked
     $.ajax({
@@ -46,6 +46,41 @@ function weatherDisplay(locationW) {
         console.log("weatherDispaly-ajax-T", responseW.list[0].main.temp);
         console.log("weatherDispaly-ajax-W", responseW.list[0].wind.speed);
         console.log("weatherDispaly-ajax-H", responseW.list[0].main.humidity);
+        console.log("icon", responseW.list[0].weather[0].icon);
+        // Creating a div to hold the city
+        var cityDiv = $("<div class='city'>");
+        
+
+
+        // Storing the temperature data converting from K to C.
+        var temp = (responseW.list[0].main.temp - 273.15).toFixed(2);
+    
+        // Creating an element to have the temperature displayed
+        var pTemp = $("<p>").text(" Temp: " + temp + " °C");
+    
+        // Displaying the temperature
+        cityDiv.append(pTemp);
+    
+        // Storing the information for wind
+        var wind = responseW.list[0].wind.speed;
+    
+        // Creating an element to hold the wind information
+        var pWind = $("<p>").text(" Wind: " + wind + " KPH");
+    
+        // Displaying the wind information
+        cityDiv.append(pWind);
+    
+        // Storing the humidity
+        var humidity = responseW.list[0].main.humidity;
+    
+        // Creating an element to hold the humidity
+        var pHumidity = $("<p>").text(" Humidity: " + humidity + " %");
+    
+        // Appending the humidity
+        cityDiv.append(pHumidity);
+    
+        // Putting the information on today's weather
+        $("#today").append(cityDiv);
     });
 }
 
